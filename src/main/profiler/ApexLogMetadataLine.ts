1d512e4d98c2090413dfbdc417f883645acfa48b
mjsmckenzie <matt@forcecreators.com>
2021-06-26 16:18:48 -0500

    Add support for other limits

--- src/main/profiler/ApexLogMetadataLine.ts
+++ src/main/profiler/ApexLogMetadataLine.ts
@@ -13,7 +13,7 @@ import constants from '../../constants';
 
 const blackList = /FLOW_START_INTERVIEW_LIMIT_USAGE/;
 const whiteList =
-  /LIMIT_USAGE|SOQL_EXECUTE_BEGIN|SOQL_EXECUTE_END|DML_BEGIN|DML_END|USER_INFO|EXECUTION_STARTED|CODE_UNIT_STARTED|METHOD_ENTRY|CODE_UNIT_FINISHED|METHOD_EXIT|FLOW_START_INTERVIEW_BEGIN|FLOW_START_INTERVIEW_END|WF_CRITERIA_BEGIN|WF_CRITERIA_END|WF_RULE_EVAL_BEGIN|WF_RULE_EVAL_END|WF_RULE_NOT_EVALUATED|FLOW_CREATE_INTERVIEW_END|FLOW_INTERVIEW_FINISHED/;
+  /LIMIT_USAGE|SOQL_EXECUTE_BEGIN|SOQL_EXECUTE_END|DML_BEGIN|DML_END|USER_INFO|EXECUTION_STARTED|CODE_UNIT_STARTED|METHOD_ENTRY|CODE_UNIT_FINISHED|METHOD_EXIT|FLOW_START_INTERVIEW_BEGIN|FLOW_START_INTERVIEW_END|WF_CRITERIA_BEGIN|WF_CRITERIA_END|WF_RULE_EVAL_BEGIN|WF_RULE_EVAL_END|WF_RULE_NOT_EVALUATED|FLOW_CREATE_INTERVIEW_END|FLOW_INTERVIEW_FINISHED|Maximum CPU |Maximum heap /;
 const startTagsWhitelist =
   /SOQL_EXECUTE_BEGIN|DML_BEGIN|CODE_UNIT_STARTED|METHOD_ENTRY|FLOW_START_INTERVIEW_BEGIN|WF_CRITERIA_BEGIN|WF_RULE_EVAL_BEGIN|FLOW_CREATE_INTERVIEW_END/;
 const endTagsWhitelist =
@@ -86,16 +86,83 @@ export default class ApexLogMetadataLine {
   }
 
   private parseLimit() {
+    console.log(this);
+    const spaceSplit = this.logLine.trimStart().split(' ');
     if (this.logLine.includes('Number of ')) {
-      const spaceSplit = this.logLine.trimStart().split(' ');
-      if (spaceSplit[2] === 'SOQL') {
-        this.limitDetail = {
-          name: constants.limitTypes.soql_queries,
-          current: parseInt(spaceSplit[4]),
-          max: parseInt(spaceSplit[7]),
-          time: ApexLogMetadataLine.LAST_REPORTED_TIME,
-        };
+      switch (spaceSplit[2]) {
+        case 'SOQL':
+          this.limitDetail = {
+            name: constants.limitTypes.soql_queries,
+            current: parseInt(spaceSplit[4]),
+            max: parseInt(spaceSplit[7]),
+            time: ApexLogMetadataLine.LAST_REPORTED_TIME,
+          };
+          break;
+        case 'query':
+          this.limitDetail = {
+            name: constants.limitTypes.soql_query_rows,
+            current: parseInt(spaceSplit[4]),
+            max: parseInt(spaceSplit[7]),
+            time: ApexLogMetadataLine.LAST_REPORTED_TIME,
+          };
+          break;
+        case 'DML':
+          this.limitDetail = {
+            name:
+              spaceSplit[3] === 'statements:'
+                ? constants.limitTypes.dml_statements
+                : constants.limitTypes.dml_rows,
+            current: parseInt(spaceSplit[4]),
+            max: parseInt(spaceSplit[7]),
+            time: ApexLogMetadataLine.LAST_REPORTED_TIME,
+          };
+          break;
+        case 'Publish' /* broken */:
+          this.limitDetail = {
+            name: constants.limitTypes.dml_push_immediate,
+            current: parseInt(spaceSplit[5]),
+            max: parseInt(spaceSplit[8]),
+            time: ApexLogMetadataLine.LAST_REPORTED_TIME,
+          };
+          break;
+        case 'callouts:':
+          this.limitDetail = {
+            name: constants.limitTypes.callouts,
+            current: parseInt(spaceSplit[3]),
+            max: parseInt(spaceSplit[6]),
+            time: ApexLogMetadataLine.LAST_REPORTED_TIME,
+          };
+          break;
+        case 'future':
+          this.limitDetail = {
+            name: constants.limitTypes.future,
+            current: parseInt(spaceSplit[4]),
+            max: parseInt(spaceSplit[7]),
+            time: ApexLogMetadataLine.LAST_REPORTED_TIME,
+          };
+          break;
+        case 'queueable':
+          this.limitDetail = {
+            name: constants.limitTypes.queueable,
+            current: parseInt(spaceSplit[8]),
+            max: parseInt(spaceSplit[11]),
+            time: ApexLogMetadataLine.LAST_REPORTED_TIME,
+          };
+          break;
+        default:
+          break;
       }
+    } else if (this.logLine.includes('Maximum ')) {
+      console.log(this.logLine);
+      this.limitDetail = {
+        name:
+          spaceSplit[1] === 'CPU'
+            ? constants.limitTypes.cpu_time
+            : constants.limitTypes.heap_size,
+        current: parseInt(spaceSplit[3]),
+        max: parseInt(spaceSplit[6]),
+        time: ApexLogMetadataLine.LAST_REPORTED_TIME,
+      };
     }
   }
 
@@ -149,8 +216,7 @@ export default class ApexLogMetadataLine {
     } else if (
       this.event === 'LIMIT_USAGE' ||
       this.logLine.includes('Number of ') ||
-      this.logLine.includes('Maximum CPU time: ') ||
-      this.logLine.includes('Maximum heap size: ')
+      this.logLine.includes('Maximum ')
     ) {
       this.type = 'limit';
     } else {
